d8348b10a2e597c3aad8868e12406480d02ffad2
Fivos <f.perakis@fysik.su.se>
2022-02-18 17:46:00 +0100

    Create cuda.sh

---
+++ slurm/cuda.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash /cfs/home/fpera/gmx_simulations/12_water/nix/cuda.nix
+#SBATCH -n6
+#SBATCH -c1
+#SBATCH --gres gpu:a100:1
+#SBATCH -pampere
+#SBATCH -Jgromacs
+
+# Automatic selection of ntomp argument based on "-c" argument to sbatch
+if [ -n "$SLURM_CPUS_PER_TASK" ]; then
+   ntomp="$SLURM_CPUS_PER_TASK"
+else
+   ntomp="1"
+fi
+
+# Make sure to set OMP_NUM_THREADS equal to the value used for ntomp
+# to avoid complaints from GROMACS
+export OMP_NUM_THREADS=$ntomp
+
+mpirun gmx_mpi mdrun -ntomp $ntomp -deffnm nvt
